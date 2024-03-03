a3041e354e4b91c3dc09da189182c5c77edc40d7
0Rorschach1 <158524039+0Rorschach1@users.noreply.github.com>
2024-03-03 12:34:56 +0330

    Create spaceship.h

---
+++ spaceship.h
@@ -0,0 +1,33 @@
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <QSet>
+//#include <QObject>
+
+
+class SpaceShip: public QGraphicsPixmapItem{
+public:
+    // methods:
+    SpaceShip(QGraphicsItem* parent = 0);
+    bool sceneEvent(QEvent *event);
+    int getWidth();
+    int getHeight();
+    void updateHealth(int h);
+    int health();
+    QTimer* speedBoostTimer;
+    int reset();
+    int speedBoost = 0;
+
+
+private:
+    QSet<int> pressedKeys;
+    QTimer* timer;
+    int health_;
+    int height;
+    int width;
+
+};
+
+#endif // SPACESHIP_H
