f88cf3fbaa8ed7b8407f9fe813776e0802f860ef
0Rorschach1 <158524039+0Rorschach1@users.noreply.github.com>
2024-03-03 12:47:19 +0330

    Create bullet.cpp

---
+++ bullet.cpp
@@ -0,0 +1,26 @@
+#include "bullet.h"
+#include <QDebug>
+#include "game.h"
+
+extern Game* game;
+
+Bullet::Bullet(QGraphicsItem* parent)
+{
+    timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this, SLOT(move()));
+    timer->start(50);
+}
+
+void Bullet::move(){
+}
+
+QTimer* Bullet::getTimer(){
+    return timer;
+}
+
+QPixmap Bullet::getPic(){
+    return pic;
+}
+void Bullet::setPic(QPixmap p){
+    pic = p;
+}
